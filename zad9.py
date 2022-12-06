4f31f149f205314ae9895e4909084609c0204453
KacperPrarat <115100470+KacperPrarat@users.noreply.github.com>
2022-12-06 23:55:55 +0100

    Update zad9.py

--- zad9.py
+++ zad9.py
@@ -6,18 +6,18 @@ for liczba in range(2,100):
             czy_pierwsza = False
     if czy_pierwsza:
         pierwsze.append(liczba)
-def factorize(n,x):
+def factorize(n):
+    x = 0
     if n > 1:
         while n% pierwsze[x] != 0:
             x = x +1
         print(pierwsze[x])
-        factorize((n/pierwsze[x]),x)
-
-
-factorize(1470,0)
-
-
-
+        factorize((n/pierwsze[x]))
 
 
 factorize(96)
+
+
+
+
+
